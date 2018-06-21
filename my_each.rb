92c6071de842b0df6dff724ee412f73b7fa38032
Ann Kim <annkim822@gmail.com>
2018-06-21 15:49:30 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
-def my_each(collection)
-  counter = 0
-  while counter < collection.length
-    yield (counter[collection])
-    counter += 1
+def my_each(array)
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i += 1
   end
-  collection
+  array
 end
